0d17b579e6b9c3b33913d2af0f9274f30e37b9e0
Kuba Jastrzębski <kuba.jastrz@outlook.com>
2018-10-24 20:40:23 +0200

    Hello world

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+import '../styles/index.scss';
+
+console.log(
+    '%cHello! ' +
+        '%cI\'m Kuba Jastrzębski ' +
+        '%c^-^ ' +
+        '%cJavaScript Developer',
+    'color: #88cdea',
+    'color: #bada55',
+    'color: #f99e61',
+    'color: #fade4b',
+    'at https://10clouds.com'
+);
+
+console.log('More things are coming soon™');
